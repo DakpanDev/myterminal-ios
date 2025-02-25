ed25393e2d671ff8f9bde27c72b58daf888d05f8
Mitchell Tol <mitchell.tol@moveagency.com>
2025-02-25 13:42:55 +0100

    Feature: added domain and data only the layers

---
+++ Data/Data.swift
@@ -0,0 +1,10 @@
+//
+//  Data.swift
+//  Data
+//
+//  Created by Mitchell Tol on 25/02/2025.
+//
+
+class Data {
+
+}
---
+++ Domain/Domain.swift
@@ -0,0 +1,10 @@
+//
+//  Domain.swift
+//  Domain
+//
+//  Created by Mitchell Tol on 25/02/2025.
+//
+
+class Domain {
+
+}
